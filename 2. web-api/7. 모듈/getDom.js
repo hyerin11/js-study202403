5ae9a74ff48e8fd4ba0e8f5015c0fe87645dfdac
hyerin11 <hodolin24@naver.com>
2024-04-04 10:50:26 +0900

    #모듈 쪼개기

---
+++ 2. web-api/7. 모듈/getDom.js
@@ -0,0 +1,6 @@
+//요소 노드 가지고 오기
+/*export*/ const $btn = document.getElementById('btn');
+/*export*/ const $pText = document.querySelector('.text');
+
+//export생략하고 한번에 써주는 방법도 있음
+export{$btn, $pText};
